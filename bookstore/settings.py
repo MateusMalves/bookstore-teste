7d0dc6a887bfbd1806a7cd1402c29b6563aab042
mateusu <mateus.amendonca@ufpe.br>
2025-05-29 02:50:44 -0300

    Corrige ALLOWED_HOSTS para domínio correto do Heroku

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-default-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ebac-bookstores-3d623cb2cdbf.herokuapp.com']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'ebac-bookstores.herokuapp.com',
+    'ebac-bookstores-3d623cb2cdbf.herokuapp.com'
+]
 
 
 # Application definition
